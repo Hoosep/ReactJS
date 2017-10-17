eed1872240a9826e52793623f2ba7e54d03d4b8e
Hoosep <hoose.muu@gmail.com>
2017-10-17 01:45:07 -0500

    Day 2: I'm gonna make a scoreboard.

--- app.jsx
+++ app.jsx
@@ -1,1 +1,37 @@
-ReactDOM.render(<h1>Hello</h1>, document.getElementById('container'));
+function Application(){
+  return (
+    <div className="scoreboard">
+      <div className="header">
+        <h1>Scoreboard</h1>
+      </div>
+      <div className="players">
+        <div className="player">
+          <div className="player-name">
+            Alexys Arias
+          </div>
+          <div classname="player-score">
+            <div className="counter">
+              <button className="counter-action decrement"> - </button>
+              <div className="counter-score"> 0 </div>
+              <button className="counter-action increment"> + </button>
+            </div>
+          </div>
+        </div>
+        <div className="player">
+          <div className="player-name">
+            José Vaca
+          </div>
+          <div classname="player-score">
+            <div className="counter">
+              <button className="counter-action decrement"> - </button>
+              <div className="counter-score"> 0 </div>
+              <button className="counter-action increment"> + </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+ReactDOM.render(<Application/>, document.getElementById('container'));
